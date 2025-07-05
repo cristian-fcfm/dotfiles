cd85c0e27b93db4ced8935cba0874cd264909308
cristian-fcfm <cristian.correa.s@ug.uchile.cl>
2025-07-05 02:26:12 -0400

    fix nvim tree conf file and agg keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,16 +4,67 @@ vim.g.maplocalleader = " "
 
 -- Atajos de teclado personalizados
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Disable the spacebar key's default behavior in Normal and Visual modes
+map({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- save file
+map('n', '<C-s>', '<cmd> w <CR>', opts)
+
+-- save file without auto-formatting
+map('n', '<leader>sn', '<cmd>noautocmd w <CR>', opts)
+
+-- quit file
+map('n', '<C-q>', '<cmd> q <CR>', opts)
+
+-- Vertical scroll and center
+map('n', '<C-d>', '<C-d>zz', opts)
+map('n', '<C-u>', '<C-u>zz', opts)
+
+-- Find and center
+map('n', 'n', 'nzzzv', opts)
+map('n', 'N', 'Nzzzv', opts)
+
+-- Buffers
+map('n', '<Tab>', ':bnext<CR>', opts)
+map('n', '<S-Tab>', ':bprevious<CR>', opts)
+map('n', '<leader>x', ':bdelete!<CR>', opts) -- close buffer
+map('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
+
+-- Window management
+map('n', '<leader>v', '<C-w>v', opts) -- split window vertically
+map('n', '<leader>h', '<C-w>s', opts) -- split window horizontally
+map('n', '<leader>se', '<C-w>=', opts) -- make split windows equal width & height
+map('n', '<leader>xs', ':close<CR>', opts) -- close current split window
+
+-- Navigate between splits
+map('n', '<C-k>', ':wincmd k<CR>', opts)
+map('n', '<C-j>', ':wincmd j<CR>', opts)
+map('n', '<C-h>', ':wincmd h<CR>', opts)
+map('n', '<C-l>', ':wincmd l<CR>', opts)
+
+-- Tabs
+map('n', '<leader>to', ':tabnew<CR>', opts) -- open new tab
+map('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
+map('n', '<leader>tn', ':tabn<CR>', opts) --  go to next tab
+map('n', '<leader>tp', ':tabp<CR>', opts) --  go to previous tab
+
+-- Toggle line wrapping
+map('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
+
+-- Stay in indent mode
+map('v', '<', '<gv', opts)
+map('v', '>', '>gv', opts)
+
+-- Keep last yanked when pasting
+map('v', 'p', '"_dP', opts)
 
 -- Abre/cierra el explorador de archivos (nvim-tree)
-map('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
 
 -- Atajos para Telescope (buscador de archivos, texto, buffers, ayuda)
-map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { noremap = true, silent = true })
-map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { noremap = true, silent = true })
-map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { noremap = true, silent = true })
-map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { noremap = true, silent = true })
-
--- Atajos generales
-vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true, desc = "Mover al panel izquierdo" })
-vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true, desc = "Mover al panel derecho" })
+map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', opts) 
+map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', opts) 
+map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', opts) 
+map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', opts) 
--- .config/nvim/lua/config/plugins/nvimtree.lua
+++ .config/nvim/lua/config/plugins/nvimtree.lua
@@ -3,25 +3,11 @@ return {
   "nvim-tree/nvim-tree.lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    local function my_on_attach(bufnr)
-      local api = require("nvim-tree.api")
-
-      -- Atajos personalizados dentro de nvim-tree
-      local opts = { buffer = bufnr, noremap = true, silent = true, nowait = true }
-
-      -- Abrir en vertical split
-      vim.keymap.set('n', 'v', api.node.open.vertical, opts)
-      -- Abrir en horizontal split
-      vim.keymap.set('n', 's', api.node.open.horizontal, opts)
-
-    end
-
     require("nvim-tree").setup({
       view = {
         width = 45,
         side = "left",
       },
-      on_attach = my_on_attach,
       actions = {
         open_file = {
           quit_on_open = false,
