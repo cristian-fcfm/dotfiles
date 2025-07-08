5f4bfe927397dad603e2d26944fce334d3d0c049
cristian-fcfm <cristian.correa.s@ug.uchile.cl>
2025-07-08 09:36:39 -0400

    change null-ls for none-ls

--- .config/nvim/lua/config/plugins/formatting.lua
+++ .config/nvim/lua/config/plugins/formatting.lua
@@ -1,27 +1,26 @@
 return {
   {
-    "nvimtools/none-ls.nvim", 
+    "nvimtools/none-ls.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      local null_ls = require("none-ls")
-      
-      null_ls.setup({
+      local none_ls = require("null-ls")
+
+      none_ls.setup({
         sources = {
           -- Python
-          null_ls.builtins.formatting.black,
-          null_ls.builtins.formatting.isort,
-          null_ls.builtins.diagnostics.flake8,
-          
+          none_ls.builtins.formatting.black,
+          none_ls.builtins.formatting.isort,
+          none_ls.builtins.diagnostics.flake8,
+
           -- JSON/YAML
-          null_ls.builtins.formatting.prettier.with({
+          none_ls.builtins.formatting.prettier.with({
             filetypes = { "json", "yaml", "markdown" },
           }),
-          null_ls.builtins.diagnostics.yamllint,
-          
+          none_ls.builtins.diagnostics.yamllint,
+
           -- Bash
-          null_ls.builtins.diagnostics.shellcheck,
-          null_ls.builtins.formatting.shfmt,
-          
+          none_ls.builtins.diagnostics.shellcheck,
+          none_ls.builtins.formatting.shfmt,
         },
         on_attach = function(client, bufnr)
           if client.supports_method("textDocument/formatting") then
