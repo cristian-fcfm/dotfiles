754bd08680ab6b9eb4d23f2d6ce281b3335bd7bb
Cristian Correa sandoval <mottaxdx@MacBook-Pro-de-Cristian.local>
2025-07-15 20:30:20 -0400

    fix plugins

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -6,6 +6,15 @@ return {
     require("nvim-treesitter.configs").setup({
       highlight = { enable = true }, -- Habilita el resaltado de sintaxis
       indent = { enable = true },    -- Habilita indentación inteligente
+      incremental_selection = {      -- Habilita selección
+        enable = true,
+        keymaps = {
+          init_selection = "<Enter>",
+          node_incremental = "<Enter>",
+          scope_incremental = false,
+          node_decremental = "<Backspace>",
+        },
+      },
     })
   end,
 }
