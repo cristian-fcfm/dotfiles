6c01bbe74973300183de49a35568bb6666532010
cristian-fcfm <cristian.correa.s@ug.uchile.cl>
2025-07-28 16:26:29 -0400

    change iron conf

--- .config/nvim/lua/config/plugins/iron.lua
+++ .config/nvim/lua/config/plugins/iron.lua
@@ -2,6 +2,7 @@ return {
   'hkupty/iron.nvim',
   config = function()
     local iron = require('iron.core')
+    local common = require('iron.fts.common')
 
     iron.setup({
       config = {
@@ -10,26 +11,34 @@ return {
         -- Your repl definitions come here
         repl_definition = {
           python = {
-            command = { 'ipython' },
+            command = { 'ipython', '--no-autoindent' },
+            format = common.bracketed_paste_python,
+            block_dividers = { '# %%', '#%%' },
           },
         },
         -- How the repl window will be displayed
         -- See below for more information
         repl_open_cmd = require('iron.view').right(60),
+        send_chunk = true,
       },
       keymaps = {
-        send_motion = '<space>rc',
-        visual_send = '<space>rc',
-        send_file = '<space>rf',
-        send_line = '<space>rl',
-        send_mark = '<space>rm',
-        mark_motion = '<space>rmc',
-        mark_visual = '<space>rmc',
-        remove_mark = '<space>rmd',
-        cr = '<space>r<cr>',
-        interrupt = '<space>r<space>',
-        exit = '<space>rq',
-        clear = '<space>rx',
+        restart_repl = '<space>rR',
+        send_motion = '<space>sc',
+        visual_send = '<space>sc',
+        send_file = '<space>sf',
+        send_line = '<space>sl',
+        send_paragraph = '<space>sp',
+        send_until_cursor = '<space>su',
+        send_mark = '<space>sm',
+        send_code_block = '<space>sb',
+        send_code_block_and_move = '<space>sn',
+        mark_motion = '<space>mc',
+        mark_visual = '<space>mc',
+        remove_mark = '<space>md',
+        cr = '<space>s<cr>',
+        interrupt = '<space>s<space>',
+        exit = '<space>sq',
+        clear = '<space>cl',
       },
       highlight = {
         italic = true,
@@ -38,7 +47,6 @@ return {
     })
 
     vim.keymap.set('n', '<space>rs', '<cmd>IronRepl<cr>')
-    vim.keymap.set('n', '<space>rr', '<cmd>IronRestart<cr>')
     vim.keymap.set('n', '<space>rF', '<cmd>IronFocus<cr>')
     vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>')
   end,
