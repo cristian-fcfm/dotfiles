a77063f61de0d16ca2ee9260b26eb563c56c3282
cristian-fcfm <cristian.correa.s@ug.uchile.cl>
2025-08-17 20:36:01 -0400

    se elimina or no uso

--- .config/nvim/lua/config/whichkey/hoversplit.lua
+++
@@ -1,9 +0,0 @@
-local wk = require('which-key')
-
-wk.add({
-  { '<leader>h', group = 'Documentación', icon = '󰈙' },
-  { '<leader>hs', function() require('hoversplit').split_remain_focused() end, desc = 'Split horizontal (foco original)', icon = '' },
-  { '<leader>hv', function() require('hoversplit').vsplit_remain_focused() end, desc = 'Split vertical (foco original)', icon = '󰤃' },
-  { '<leader>hS', function() require('hoversplit').split() end, desc = 'Split horizontal (cambiar foco)', icon = '' },
-  { '<leader>hV', function() require('hoversplit').vsplit() end, desc = 'Split vertical (cambiar foco)', icon = '󰤃' },
-})
