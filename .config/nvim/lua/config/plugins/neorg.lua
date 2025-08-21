f326a65db8b2a9fba87bfb6c1e354782769e73dd
Cristian Correa sandoval <mottaxdx@MacBook-Pro-de-Cristian.local>
2025-08-21 18:19:34 -0400

    se agregar configuracion para journal

--- .config/nvim/lua/config/plugins/neorg.lua
+++ .config/nvim/lua/config/plugins/neorg.lua
@@ -17,10 +17,22 @@ return {
                     config = {
                         workspaces = {
                             notes = "~/Documents/notes/",
+                            reviews = "~/Documents/notes/0. Reviews/",
+                            projects = "~/Documents/notes/1. Projects/",
+                            areas = "~/Documents/notes/2. Areas/",
+                            resources = "~/Documents/notes/3. Resources/",
                         },
                         default_workspace = "notes",
                     },
+
                 },
+                ["core.journal"] = {
+                    config = {
+                        journal_folder = "0.4. Daily",
+                        strategy = "flat",
+                        workspace = "reviews"
+                    }
+                }
             },
         }
 
--- .config/nvim/lua/config/whichkey/notes.lua
+++ .config/nvim/lua/config/whichkey/notes.lua
@@ -6,6 +6,9 @@ wk.add({
     -- Neorg workspace commands
     { '<leader>nw', group = 'Workspace', icon = '󰬱' },
     { '<leader>nws', ':Neorg workspace notes<CR>', desc = 'Switch to notes workspace', icon = '' },
+    { '<leader>nwp', ':Neorg workspace projects<CR>', desc = 'Switch to projects workspace', icon = '' },
+    { '<leader>nwa', ':Neorg workspace areas<CR>', desc = 'Switch to areas workspace', icon = '' },
+    { '<leader>nwr', ':Neorg workspace resources<CR>', desc = 'Switch to resources workspace', icon = '' },
 
     -- Neorg hop-link commands
     { '<leader>nh', group = 'Hop Links', icon = '󱅷' },
@@ -13,4 +16,10 @@ wk.add({
     { '<leader>nhv', '<Plug>(neorg.esupports.hop.hop-link.vsplit)', desc = 'Follow link in vertical split', icon = '󰲔' },
     { '<leader>nht', '<Plug>(neorg.esupports.hop.hop-link.tab-drop)', desc = 'Follow link in new tab', icon = '󰴛' },
     { '<leader>nhd', '<Plug>(neorg.esupports.hop.hop-link.drop)', desc = 'Follow link in existing buffer', icon = '󱄁' },
+
+    -- Neorg Journal
+    { '<leader>nj', group = 'Journal', icon = '' },
+    { '<leader>njt', ':Neorg journal today<CR>', desc = 'generar nota para hoy', icon = '' },
+    { '<leader>njy', ':Neorg journal yesterday<CR>', desc = 'generar nota para ayer', icon = '' },
+    { '<leader>njm', ':Neorg journal tomorrow<CR>', desc = 'generar nota para manana', icon = '' },
 })
