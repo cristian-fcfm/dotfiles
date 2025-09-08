9c17f1e8c73f6fdff952cd1e2d254e15dbdd6193
cristian-fcfm <cristian.correa.s@ug.uchile.cl>
2025-09-08 05:56:07 -0300

    se agregan iconos faltantes de neorg

--- .config/nvim/lua/config/whichkey/notes.lua
+++ .config/nvim/lua/config/whichkey/notes.lua
@@ -18,31 +18,31 @@ wk.add({
     { '<leader>nhd', '<Plug>(neorg.esupports.hop.hop-link.drop)', desc = 'Follow link in existing buffer', icon = '󱄁' },
 
     -- Neorg Journal
-    { '<leader>nj', group = 'Journal', icon = '' },
-    { '<leader>njt', ':Neorg journal today<CR>', desc = 'generar nota para hoy', icon = '' },
-    { '<leader>njy', ':Neorg journal yesterday<CR>', desc = 'generar nota para ayer', icon = '' },
-    { '<leader>njm', ':Neorg journal tomorrow<CR>', desc = 'generar nota para manana', icon = '' },
+    { '<leader>nj', group = 'Journal', icon = '' },
+    { '<leader>njt', ':Neorg journal today<CR>', desc = 'Create today note', icon = '󰃶' },
+    { '<leader>njy', ':Neorg journal yesterday<CR>', desc = 'Create yesterday note', icon = '' },
+    { '<leader>njm', ':Neorg journal tomorrow<CR>', desc = 'Create tomorrow note', icon = '' },
 
     -- Neorg pivot
-    { '<leader>np', group = 'Pivot', icon = '' },
-    { '<leader>npt', '<Plug>(neorg.pivot.list.toggle)', desc = 'List toggle', icon = '' },
-    { '<leader>npi', '<Plug>(neorg.pivot.list.invert)', desc = 'List invert', icon = '' },
+    { '<leader>np', group = 'Pivot', icon = '' },
+    { '<leader>npt', '<Plug>(neorg.pivot.list.toggle)', desc = 'List toggle', icon = '' },
+    { '<leader>npi', '<Plug>(neorg.pivot.list.invert)', desc = 'List invert', icon = '' },
 
     -- Neorg promo
-    { '<leader>nr', group = 'Promote/Demote', icon = '' },
-    { '<leader>nrp', '<Plug>(neorg.promo.promote)', desc = 'Promote item on current line', icon = '' },
-    { '<leader>nrP', '<Plug>(neorg.promo.promote.nested)', desc = 'Promote current line and nested items', icon = '' },
-    { '<leader>nrr', '<Plug>(neorg.promo.promote.range)', desc = 'Promote all items in range', icon = '' },
-    { '<leader>nrd', '<Plug>(neorg.promo.demote)', desc = 'Demote item on current line', icon = '' },
-    { '<leader>nrD', '<Plug>(neorg.promo.demote.nested)', desc = 'Demote current line and nested items', icon = '' },
-    { '<leader>nrR', '<Plug>(neorg.promo.demote.range)', desc = 'Demote all items in range', icon = '' },
+    { '<leader>nr', group = 'Promote/Demote', icon = '' },
+    { '<leader>nrp', '<Plug>(neorg.promo.promote)', desc = 'Promote item on current line', icon = '󰉶' },
+    { '<leader>nrP', '<Plug>(neorg.promo.promote.nested)', desc = 'Promote current line and nested items', icon = '󰉶' },
+    { '<leader>nrr', '<Plug>(neorg.promo.promote.range)', desc = 'Promote all items in range', icon = '󰉶' },
+    { '<leader>nrd', '<Plug>(neorg.promo.demote)', desc = 'Demote item on current line', icon = '󰉵' },
+    { '<leader>nrD', '<Plug>(neorg.promo.demote.nested)', desc = 'Demote current line and nested items', icon = '󰉵' },
+    { '<leader>nrR', '<Plug>(neorg.promo.demote.range)', desc = 'Demote all items in range', icon = '󰉵' },
 
     -- Neorg TOC
-    { '<leader>nt', group = 'Table of Contents', icon = '' },
-    { '<leader>ntl', ':Neorg toc left<CR>', desc = 'Open TOC on left side', icon = '' },
-    { '<leader>ntr', ':Neorg toc right<CR>', desc = 'Open TOC on right side', icon = '' },
-    { '<leader>ntq', ':Neorg toc qflist<CR>', desc = 'Send TOC to quickfix list', icon = '' },
-    { '<leader>ntt', ':Neorg toc<CR>', desc = 'Open TOC', icon = '' },
+    { '<leader>nt', group = 'Table of Contents', icon = '󰠶' },
+    { '<leader>ntl', ':Neorg toc left<CR>', desc = 'Open TOC on left side', icon = '' },
+    { '<leader>ntr', ':Neorg toc right<CR>', desc = 'Open TOC on right side', icon = '' },
+    { '<leader>ntq', ':Neorg toc qflist<CR>', desc = 'Send TOC to quickfix list', icon = '' },
+    { '<leader>ntt', ':Neorg toc<CR>', desc = 'Open TOC', icon = '' },
 
     -- Neorg TODO items
     { '<leader>nd', group = 'TODO Items', icon = '☑' },
@@ -53,13 +53,13 @@ wk.add({
     { '<leader>ndc', '<Plug>(neorg.qol.todo-items.todo.task-cancelled)', desc = 'Mark task as cancelled', icon = '✗' },
     { '<leader>ndr', '<Plug>(neorg.qol.todo-items.todo.task-recurring)', desc = 'Mark task as recurring', icon = '⟲' },
     { '<leader>ndi', '<Plug>(neorg.qol.todo-items.todo.task-important)', desc = 'Mark task as important', icon = '!' },
-    { '<leader>nds', '<Plug>(neorg.qol.todo-items.todo.task-cycle)', desc = 'Cycle task state', icon = '🔄' },
-    { '<leader>ndS', '<Plug>(neorg.qol.todo-items.todo.task-cycle-reverse)', desc = 'Cycle task state reverse', icon = '🔄' },
+    { '<leader>nds', '<Plug>(neorg.qol.todo-items.todo.task-cycle)', desc = 'Cycle task state', icon = '' },
+    { '<leader>ndS', '<Plug>(neorg.qol.todo-items.todo.task-cycle-reverse)', desc = 'Cycle task state reverse', icon = '' },
 
     -- Templates
-    { '<leader>ne', group = 'Templates', icon = '📄' },
-    { '<leader>ner', function() require('config.neorg.neorg-templater').replace_with_template() end, desc = 'Reemplazar con template', icon = '🔄' },
-    { '<leader>nea', function() require('config.neorg.neorg-templater').append_template() end, desc = 'Añadir template', icon = '➕' },
+    { '<leader>ne', group = 'Templates', icon = '' },
+    { '<leader>ner', function() require('config.neorg.neorg-templater').replace_with_template() end, desc = 'Replace template', icon = '󰬲' },
+    { '<leader>nea', function() require('config.neorg.neorg-templater').append_template() end, desc = 'Add template last line', icon = '' },
 
     -- Folding
     { '<leader>nf', group = 'Folding', icon = '󰘖' },
