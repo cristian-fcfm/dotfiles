acdd1676a46d11a61dd6bbe6f966bcd458975cae
cristian-fcfm <cristian.correa.s@ug.uchile.cl>
2025-10-22 09:25:18 -0300

    se modifica lsp para que el diagnostico sea mas pequeno

--- .config/nvim/lua/config/plugins/lsp.lua
+++ .config/nvim/lua/config/plugins/lsp.lua
@@ -126,19 +126,9 @@ return {
             vim.diagnostic.config {
                 severity_sort = true,
                 float = { border = 'rounded', source = 'if_many' },
-                underline = { severity = vim.diagnostic.severity.ERROR },
-                signs = {
-                    text = {
-                        [vim.diagnostic.severity.ERROR] = '󰅚 ',
-                        [vim.diagnostic.severity.WARN] = '󰀪 ',
-                        [vim.diagnostic.severity.INFO] = '󰋽 ',
-                        [vim.diagnostic.severity.HINT] = '󰌶 ',
-                    },
-                },
-                virtual_text = {
-                    source = 'if_many',
-                    spacing = 2,
-                },
+                underline = true,
+                signs = false,
+                virtual_text = false,
             }
 
             -- Configuración de cada LSP
