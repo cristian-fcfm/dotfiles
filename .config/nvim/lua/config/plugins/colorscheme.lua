453487ba63ec40242d921edd7a72472db409a1e1
Cristian Correa sandoval <mottaxdx@MacBook-Pro-de-Cristian.local>
2025-11-03 17:32:43 -0300

    fix: para mayor visibilidad se cambia color de la barra de numeros

--- .config/nvim/lua/config/plugins/colorscheme.lua
+++ .config/nvim/lua/config/plugins/colorscheme.lua
@@ -3,6 +3,15 @@ return {
   "folke/tokyonight.nvim",
   priority = 1000, -- Se carga primero para que el tema se aplique antes que otros plugins
   config = function()
-    vim.cmd.colorscheme("tokyonight") -- Aplica el tema Tokyonight
+    require("tokyonight").setup({
+      style = "night",
+      on_highlights = function(hl, c)
+        hl.LineNr = { fg = c.purple, bg = c.bg_gutter }
+        hl.LineNrAbove = { fg = c.purple, bg = c.bg_gutter }
+        hl.LineNrBelow = { fg = c.purple, bg = c.bg_gutter }
+        hl.CursorLineNr = { fg = "#ff9e64", bold = true, bg = c.bg_gutter }
+      end,
+    })
+    vim.cmd.colorscheme("tokyonight")
   end,
 }
