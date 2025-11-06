489cc4cd939c7c811becd4f54592d89e6e32b0a7
cristian-fcfm <cristian.correa.s@ug.uchile.cl>
2025-11-06 16:17:28 -0300

    feat: configure blink.cmp with LuaSnip preset
    Replace deprecated luasnip provider with new snippets preset system

--- .config/nvim/lua/config/plugins/completion.lua
+++ .config/nvim/lua/config/plugins/completion.lua
@@ -6,9 +6,52 @@ return {
     opts = {},
   },
   {
+    "L3MON4D3/LuaSnip",
+    version = "v2.*",
+    build = "make install_jsregexp",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+    },
+    config = function()
+      local luasnip = require("luasnip")
+
+      -- Cargar snippets de friendly-snippets
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      -- Cargar tus snippets personalizados
+      require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/snippets" })
+
+      luasnip.config.setup({
+        history = true,
+        updateevents = "TextChanged,TextChangedI",
+        enable_autosnippets = true,
+      })
+
+      -- Keymaps para navegar entre placeholders
+      vim.keymap.set({ "i", "s" }, "<C-l>", function()
+        if luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        end
+      end, { silent = true, desc = "LuaSnip: Expand or jump forward" })
+
+      vim.keymap.set({ "i", "s" }, "<C-h>", function()
+        if luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        end
+      end, { silent = true, desc = "LuaSnip: Jump backward" })
+
+      vim.keymap.set({ "i", "s" }, "<C-j>", function()
+        if luasnip.choice_active() then
+          luasnip.change_choice(1)
+        end
+      end, { silent = true, desc = "LuaSnip: Change choice" })
+    end,
+  },
+  {
     "saghen/blink.cmp",
     dependencies = {
       "rafamadriz/friendly-snippets",
+      "L3MON4D3/LuaSnip",
     },
     version = "1.*",
     event = { "InsertEnter", "CmdlineEnter" },
@@ -60,6 +103,10 @@ return {
             min_keyword_length = 4,
           },
         },
+      },
+
+      snippets = {
+        preset = "luasnip",
       },
 
       cmdline = {
