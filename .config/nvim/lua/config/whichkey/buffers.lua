b9f246eb6e53674d38c6a335f221348c2dbaa594
cristian-fcfm <cristian.correa.s@ug.uchile.cl>
2025-11-18 02:01:58 -0300

    feat: add buffer navigation keymaps
    - Add Shift+h/l for previous/next buffer
    - Add <leader>bD for force delete buffer
    - Keep existing Tab/Shift+Tab mappings

--- .config/nvim/lua/config/whichkey/buffers.lua
+++ .config/nvim/lua/config/whichkey/buffers.lua
@@ -4,11 +4,15 @@ wk.add({
   { "<leader>b", group = "Buffers", icon = "󰓩" },
   { "<leader>bn", "<cmd>enew<CR>", desc = "New buffer", icon = "󰝜" },
   { "<leader>bd", "<cmd>bprevious | bdelete! #<CR>", desc = "Delete buffer", icon = "󰭌" },
+  { "<leader>bD", "<cmd>bdelete!<CR>", desc = "Force delete buffer", icon = "󰭌" },
   { "<leader>bp", "<cmd>BufferLinePick<CR>", desc = "Pick buffer", icon = "󰒅" },
   { "<leader>bc", "<cmd>BufferLinePickClose<CR>", desc = "Pick & close", icon = "󱟁" },
   { "<leader>bh", "<cmd>BufferLineCloseLeft<CR>", desc = "Close left buffers", icon = "󰅖" },
   { "<leader>bl", "<cmd>BufferLineCloseRight<CR>", desc = "Close right buffers", icon = "󰅖" },
   { "<leader>bo", "<cmd>BufferLineCloseOthers<CR>", desc = "Close other buffers", icon = "󰈴" },
+  -- Buffer navigation
   { "<Tab>", "<cmd>BufferLineCycleNext<CR>", desc = "Next buffer", icon = "󰒭" },
   { "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", desc = "Previous buffer", icon = "󰒮" },
+  { "<S-l>", "<cmd>BufferLineCycleNext<CR>", desc = "Next buffer", icon = "󰒭" },
+  { "<S-h>", "<cmd>BufferLineCyclePrev<CR>", desc = "Previous buffer", icon = "󰒮" },
 })
