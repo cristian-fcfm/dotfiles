9570e3329ec91b26c7bdf2e02a9226233b6b59df
Cristian Correa sandoval <mottaxdx@MacBook-Pro-de-Cristian.local>
2025-11-25 04:55:26 -0300

    feat: actualizar whichkey de zk con nuevas funciones
    - Keybindings para monthly y yearly notes
    - Keybindings para projects, areas, resources y archives
    - Integración completa con zktools.projects
    - Actualizar paths a formato con guiones (0-reviews, etc)

--- .config/nvim/lua/config/whichkey/zk.lua
+++ .config/nvim/lua/config/whichkey/zk.lua
@@ -137,7 +137,7 @@ wk.add({
   {
     "<leader>zdo",
     function()
-      require("zk").edit({ hrefs = { "0.reviews/4.daily" } }, { multi_select = false })
+      require("zk").edit({ hrefs = { "0-reviews/4-daily" } }, { multi_select = false })
     end,
     desc = "[O]pen daily note",
     icon = "󱨋",
@@ -156,10 +156,166 @@ wk.add({
   {
     "<leader>zwo",
     function()
-      require("zk").edit({ hrefs = { "0.reviews/3.weekly" } }, { multi_select = false })
+      require("zk").edit({ hrefs = { "0-reviews/3-weekly" } }, { multi_select = false })
     end,
     desc = "[O]pen weekly note",
     icon = "󱨋",
+  },
+
+  -- Monthly notes
+  { "<leader>zm", group = "[M]onthly notes", icon = "󰸗" },
+  {
+    "<leader>zmn",
+    function()
+      require("zktools.dates").create_monthly_note()
+    end,
+    desc = "[N]ew monthly note",
+    icon = "󱘒",
+  },
+  {
+    "<leader>zmo",
+    function()
+      require("zk").edit({ hrefs = { "0-reviews/2-monthly" } }, { multi_select = false })
+    end,
+    desc = "[O]pen monthly note",
+    icon = "󱨋",
+  },
+
+  -- Yearly notes
+  { "<leader>zy", group = "[Y]early notes", icon = "󰸘" },
+  {
+    "<leader>zyn",
+    function()
+      require("zktools.dates").create_yearly_note()
+    end,
+    desc = "[N]ew yearly note",
+    icon = "󱘒",
+  },
+  {
+    "<leader>zyo",
+    function()
+      require("zk").edit({ hrefs = { "0-reviews/1-yearly" } }, { multi_select = false })
+    end,
+    desc = "[O]pen yearly note",
+    icon = "󱨋",
+  },
+
+  -- Projects
+  { "<leader>zp", group = "[P]rojects", icon = "󰸕" },
+  {
+    "<leader>zpn",
+    function()
+      require("zktools.projects").create_project_interactive()
+    end,
+    desc = "[N]ew project",
+    icon = "󱘒",
+  },
+  {
+    "<leader>zpo",
+    function()
+      require("zktools.projects").open_projects()
+    end,
+    desc = "[O]pen project",
+    icon = "󱨋",
+  },
+  {
+    "<leader>zps",
+    function()
+      require("zktools.projects").search_projects()
+    end,
+    desc = "[S]earch projects",
+    icon = "󱙓",
+  },
+  {
+    "<leader>zpm",
+    function()
+      require("zktools.projects").create_meeting_interactive()
+    end,
+    desc = "New [M]eeting",
+    icon = "󰥔",
+  },
+
+  -- Areas
+  { "<leader>za", group = "[A]reas", icon = "󰹕" },
+  {
+    "<leader>zan",
+    function()
+      require("zktools.projects").create_area()
+    end,
+    desc = "[N]ew area",
+    icon = "󱘒",
+  },
+  {
+    "<leader>zao",
+    function()
+      require("zktools.projects").open_areas()
+    end,
+    desc = "[O]pen areas",
+    icon = "󱨋",
+  },
+
+  -- Resources
+  { "<leader>zr", group = "[R]esources", icon = "󰠮" },
+  {
+    "<leader>zrn",
+    function()
+      require("zktools.projects").create_resource()
+    end,
+    desc = "[N]ew resource",
+    icon = "󱘒",
+  },
+  {
+    "<leader>zrz",
+    function()
+      require("zktools.projects").create_zettel()
+    end,
+    desc = "New [Z]ettel",
+    icon = "󱞁",
+  },
+  {
+    "<leader>zro",
+    function()
+      require("zktools.projects").open_resources()
+    end,
+    desc = "[O]pen resources",
+    icon = "󱨋",
+  },
+  {
+    "<leader>zrl",
+    function()
+      require("zk").pick_notes({ tags = { "zettel" } }, { multi_select = false }, function(notes)
+        require("zk").insert_link(notes)
+      end)
+    end,
+    desc = "[L]ink to zettel",
+    icon = "󰹕",
+  },
+  {
+    "<leader>zrg",
+    function()
+      require("zk").edit({ tags = { "zettel" }, linkedBy = { vim.api.nvim_buf_get_name(0) } }, { multi_select = false })
+    end,
+    desc = "Zettel [G]raph (backlinks)",
+    icon = "󰘦",
+  },
+
+  -- Archives
+  { "<leader>zx", group = "Archi[x]e", icon = "󰮁" },
+  {
+    "<leader>zxo",
+    function()
+      require("zktools.projects").open_archives()
+    end,
+    desc = "[O]pen archives",
+    icon = "󱨋",
+  },
+  {
+    "<leader>zxa",
+    function()
+      require("zktools.projects").archive_current_note()
+    end,
+    desc = "[A]rchive current",
+    icon = "󰮁",
   },
 
   -- Habits tracker
