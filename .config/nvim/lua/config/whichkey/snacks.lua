1ea97e211c7da3597b15daa8e0f87b00c1ec9080
cristian-fcfm <cristian.correa.s@ug.uchile.cl>
2025-12-05 22:48:11 -0300

    refactor(nvim): definir funciones explícitas en keybindings de snacks
    - Cambiar de descriptores simples a funciones explícitas
    - Mejorar claridad de llamadas a Snacks para zen, zoom, explorer y dashboard
    - Mantener consistencia con otras configuraciones de whichkey

--- .config/nvim/lua/config/whichkey/snacks.lua
+++ .config/nvim/lua/config/whichkey/snacks.lua
@@ -2,8 +2,36 @@ local wk = require("which-key")
 
 wk.add({
   { "<leader>z", group = "Zen", icon = "󰚀" },
-  { "<leader>zz", desc = "Toggle Zen Mode", icon = "󰚀" },
-  { "<leader>zZ", desc = "Toggle Zoom", icon = "" },
-  { "<leader>e", desc = "Toggle Explorer", icon = "󰙅" },
-  { "<leader>.", desc = "Dashboard", icon = "" },
+  {
+    "<leader>zz",
+    function()
+      Snacks.zen()
+    end,
+    desc = "Toggle Zen Mode",
+    icon = "󰚀",
+  },
+  {
+    "<leader>zZ",
+    function()
+      Snacks.zen.zoom()
+    end,
+    desc = "Toggle Zoom",
+    icon = "",
+  },
+  {
+    "<leader>e",
+    function()
+      Snacks.explorer()
+    end,
+    desc = "Toggle Explorer",
+    icon = "󰙅",
+  },
+  {
+    "<leader>.",
+    function()
+      Snacks.dashboard()
+    end,
+    desc = "Dashboard",
+    icon = "",
+  },
 })
