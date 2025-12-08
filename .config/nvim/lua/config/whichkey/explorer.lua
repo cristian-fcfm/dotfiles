7b865ff30a443d28d9e7a28e976d6368b9024b56
cristian-fcfm <cristian.correa.s@ug.uchile.cl>
2025-12-08 04:44:15 -0300

    refactor(which-key): actualizar keybindings de buffers, explorer y git

--- .config/nvim/lua/config/whichkey/explorer.lua
+++ .config/nvim/lua/config/whichkey/explorer.lua
@@ -1,20 +1,6 @@
 local wk = require("which-key")
 
 wk.add({
-  -- Oil.nvim (buffer-style file manager)
-  {
-    ".",
-    "<cmd>Oil<cr>",
-    desc = "Oil (normal)",
-    icon = "󰏇",
-  },
-  {
-    "--",
-    "<cmd>Oil --float<cr>",
-    desc = "Oil (float)",
-    icon = "󰏇",
-  },
-
   -- Snacks Explorer (tree-style file manager)
   {
     "<leader>e",
