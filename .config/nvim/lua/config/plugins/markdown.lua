bd05c4c4062fb286bf4c881d7f42558227196d9f
cristian-fcfm <cristian.correa.s@ug.uchile.cl>
2025-12-08 04:44:27 -0300

    refactor(plugins): actualizar configuración de livepreview, markdown, todocomments y whichkey

--- .config/nvim/lua/config/plugins/markdown.lua
+++ .config/nvim/lua/config/plugins/markdown.lua
@@ -1,20 +1,17 @@
 return {
-  -- Markdown preview and rendering
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    ft = { "markdown" },
-    opts = {
-      file_types = { "markdown" },
-      code = {
-        sign = false,
-        width = "block",
-        right_pad = 1,
-      },
-      heading = {
-        sign = false,
-        icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
-      },
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  ft = { "markdown" },
+  opts = {
+    file_types = { "markdown" },
+    code = {
+      sign = false,
+      width = "block",
+      right_pad = 1,
+    },
+    heading = {
+      sign = false,
+      icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
     },
   },
 }
