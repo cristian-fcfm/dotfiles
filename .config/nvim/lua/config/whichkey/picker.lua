ada750579228867c6a8d39c0bf93d5c02f9412d5
cristian-fcfm <cristian.correa.s@ug.uchile.cl>
2025-12-08 04:43:58 -0300

    refactor(which-key): reorganizar archivos de pickers y find

---
+++ .config/nvim/lua/config/whichkey/picker.lua
@@ -0,0 +1,60 @@
+local wk = require("which-key")
+
+wk.add({
+  {
+    "<leader>fh",
+    function()
+      Snacks.picker.help()
+    end,
+    desc = "[F]ind [H]elp",
+    icon = "󰋖",
+  },
+  {
+    "<leader>fk",
+    function()
+      Snacks.picker.keymaps()
+    end,
+    desc = "[F]ind [K]eymaps",
+    icon = "",
+  },
+  {
+    "<leader>fd",
+    function()
+      Snacks.picker.diagnostics_buffer()
+    end,
+    desc = "[F]ind [D]iagnostics in buffers",
+    icon = "󰩂",
+  },
+  {
+    "<leader>fD",
+    function()
+      Snacks.picker.diagnostics()
+    end,
+    desc = "[F]ind [D]iagnostics in project",
+    icon = "󰩂",
+  },
+  {
+    "<leader>fi",
+    function()
+      Snacks.picker.icons()
+    end,
+    desc = "[F]ind [I]cons",
+    icon = "󰀻",
+  },
+  {
+    "<leader>fn",
+    function()
+      Snacks.picker.notifications()
+    end,
+    desc = "[F]ind [N]otifications",
+    icon = "󰵅",
+  },
+  {
+    "<leader>fy",
+    function()
+      Snacks.picker.cliphist()
+    end,
+    desc = "[F]ind [Y]ank history",
+    icon = "󰋚",
+  },
+})
