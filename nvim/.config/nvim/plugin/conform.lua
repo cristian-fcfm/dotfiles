da22bea7474e7d3b184df2c112012f266a023d6b
cristian-fcfm <cristian.correa.s@ug.uchile.cl>
2026-04-04 02:21:24 -0300

    feat(nvim): add conform.nvim via vim.pack

---
+++ nvim/.config/nvim/plugin/conform.lua
@@ -0,0 +1,67 @@
+vim.schedule(function()
+  vim.pack.add({
+    { src = "https://github.com/stevearc/conform.nvim", version = vim.version.range("*") },
+  })
+
+  local utils = require("utils")
+  local formatters_by_ft = {}
+
+  if utils.executable("ruff") then
+    formatters_by_ft.python = { "ruff_format", "ruff_organize_imports" }
+  end
+
+  if utils.executable("prettier") then
+    formatters_by_ft.json = { "prettier" }
+    formatters_by_ft.yaml = { "prettier" }
+    formatters_by_ft.markdown = { "prettier" }
+    formatters_by_ft.zk = { "prettier" }
+    formatters_by_ft.html = { "prettier" }
+    formatters_by_ft.css = { "prettier" }
+    formatters_by_ft.scss = { "prettier" }
+    formatters_by_ft.less = { "prettier" }
+  end
+
+  if utils.executable("shfmt") then
+    formatters_by_ft.bash = { "shfmt" }
+    formatters_by_ft.sh = { "shfmt" }
+  end
+
+  if utils.executable("stylua") then
+    formatters_by_ft.lua = { "stylua" }
+  end
+
+  if utils.executable("rustfmt") then
+    formatters_by_ft.rust = { "rustfmt" }
+  end
+
+  formatters_by_ft.typst = { "lsp_format" }
+
+  require("conform").setup({
+    formatters_by_ft = formatters_by_ft,
+    format_on_save = function(bufnr)
+      return {
+        timeout_ms = 2000,
+        lsp_format = "fallback",
+      }, function(err)
+        if err then
+          vim.notify("Error al formatear: " .. tostring(err), vim.log.levels.ERROR, { title = "Conform" })
+        else
+          vim.notify("✓ Formateado exitosamente", vim.log.levels.INFO, { title = "Conform" })
+        end
+      end
+    end,
+    formatters = {
+      shfmt = { prepend_args = { "-i", "2" } },
+      ruff_format = {
+        command = "ruff",
+        args = { "format", "--stdin-filename", "$FILENAME", "-" },
+        stdin = true,
+      },
+      ruff_organize_imports = {
+        command = "ruff",
+        args = { "check", "--select", "I", "--fix", "--stdin-filename", "$FILENAME", "-" },
+        stdin = true,
+      },
+    },
+  })
+end)
