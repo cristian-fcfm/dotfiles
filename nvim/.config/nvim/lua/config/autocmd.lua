4f7df6ed18cd7ffd457f10da929a6ac8384b3476
Cristian Correa <ccorreas@CLRTLGCX1T9WK9N.local>
2026-05-14 18:10:19 -0400

    feat(nvim): agregar quickfix/loclist keymaps y corregir spell/indent markdown
    - Agregar <leader>ll/lq/lc/lL para quickfix y loclist
    - Forzar tabstop/shiftwidth 2 en markdown/typst/zk
    - Eliminar remaps rotos de vim.spell.goto_next/prev

--- nvim/.config/nvim/lua/config/autocmd.lua
+++ nvim/.config/nvim/lua/config/autocmd.lua
@@ -110,7 +110,10 @@ api.nvim_create_autocmd("FileType", {
   desc = "Activar spell en filetypes de escritura",
   callback = function()
     vim.opt_local.spell = true
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
     vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
   end,
 })
 
